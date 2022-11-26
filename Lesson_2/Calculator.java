c65866be53fdea5d2941c7d2e5fbd73c3f20c6b2
Stanislav Soharev <devsoharev@yandex.by>
2022-11-26 13:54:02 +0300

    hw Lesson_2. Calculator. Corregting notes

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -1,34 +1,22 @@
 public class Calculator {
-    char sign;
-    int num1;
-    int num2;
-
-    public char getSign() {
-        return sign;
-    }
+    private char sign;
+    private int num1;
+    private int num2;
 
     public void setSign(char sign) {
         this.sign = sign;
-    }
-
-    public int getNum1() {
-        return num1;
     }
 
     public void setNum1(int num1) {
         this.num1 = num1;
     }
 
-    public int getNum2() {
-        return num2;
-    }
-
     public void setNum2(int num2) {
         this.num2 = num2;
     }
 
-    public int calc(char sign, int num1, int num2) {
-        int result;
+    public int calc() {
+        int result = 1;
         switch (sign) {
             case '+' -> result = num1 + num2;
             case '-' -> result = num1 - num2;
@@ -36,15 +24,13 @@ public class Calculator {
             case '/' -> result = num1 / num2;
             case '%' -> result = num1 % num2;
             case '^' -> {
-                int r = 1;
                 for (int i = 1; i <= num2; i++) {
-                    r *= num1;
+                    result *= num1;
                 }
-                result = r;
             }
             default -> {
                 System.out.println("Вам нужен другой калькулятор");
-                result = -1;
+                result = 0;
             }
         }
         return result;
