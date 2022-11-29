f69e4504080cacc6e3736faf67d1ae7269854c0d
Stanislav Soharev <devsoharev@yandex.by>
2022-11-29 14:41:48 +0300

    hw Lesson_2. GuessNumber. Correcting notes_3

--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class GuessNumberTest {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in, "cp866");
         System.out.println("Игрок 1 введите свое имя");
         Player player1 = new Player(sc.nextLine());
         System.out.println("Игрок 2 введите свое имя");
